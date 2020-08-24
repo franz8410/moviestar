8f52af5fc89527daf779a1b58bae6a14c4f2c9d2
franz <xsonyun1@gmail.com>
2020-08-24 22:23:29 +0900

    푸시 테스트

--- app.py
+++ app.py
@@ -53,6 +53,7 @@ def web_hook():
     print(web_hook_data)
     os.system('cd /hmoe/ubuntu/moviestar && git pull')
     return jsonify({'result': 'success'})
+# push 테스트용
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
